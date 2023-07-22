35d7d256ebfce7ccb048bae07c8058d9d3490122
hadiaHani <hh7113404@gmail.com>
2023-07-22 22:30:08 +0300

    add id, name to cache

--- lib/core/cache/cache.dart
+++ lib/core/cache/cache.dart
@@ -1,1 +1,20 @@
-class CacheData {}
+class CacheData {
+  static String? name;
+  static String? productID;
+
+  void setName(String value) {
+    CacheData.name = value;
+  }
+
+  String? getName() {
+    return name;
+  }
+
+  void setProductID(String value) {
+    CacheData.productID = value;
+  }
+
+  String? getProductID() {
+    return productID;
+  }
+}
