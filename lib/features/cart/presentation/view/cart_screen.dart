7d0a76d81f4a2efafc1354ea32fff9fb155f0883
hadiaHani <hh7113404@gmail.com>
2023-07-22 22:37:07 +0300

    complete CartScreen

--- lib/features/cart/presentation/view/cart_screen.dart
+++ lib/features/cart/presentation/view/cart_screen.dart
@@ -1,11 +1,15 @@
+import 'package:ecommerce_admin_app_firebase/config/constant.dart';
 import 'package:ecommerce_admin_app_firebase/core/resources/manager_assets.dart';
 import 'package:ecommerce_admin_app_firebase/core/resources/manager_size.dart';
 import 'package:ecommerce_admin_app_firebase/core/resources/manager_strings.dart';
 import 'package:ecommerce_admin_app_firebase/core/widgets/empty_bag.dart';
+import 'package:ecommerce_admin_app_firebase/core/widgets/show_error_dialog.dart';
 import 'package:ecommerce_admin_app_firebase/core/widgets/title_text.dart';
+import 'package:ecommerce_admin_app_firebase/features/cart/presentation/controller/cart_provider.dart';
 import 'package:ecommerce_admin_app_firebase/features/cart/presentation/view/widgets/bottom_checkout.dart';
 import 'package:ecommerce_admin_app_firebase/features/cart/presentation/view/widgets/cart_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -13,7 +17,8 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return isEmpty
+    final cartProvider = Provider.of<CartProvider>(context);
+    return cartProvider.getCartItems.isEmpty
         ? Scaffold(
             body: EmptyBagWidget(
                 imagePath: ManagerAssets.shoppingBasket,
@@ -24,7 +29,8 @@ class CartScreen extends StatelessWidget {
         : Scaffold(
             bottomSheet: const CartBottomCheckout(),
             appBar: AppBar(
-              title: const TitlesTextWidget(label: "Cart (5)"),
+              title: TitlesTextWidget(
+                  label: "Cart (${cartProvider.getCartItems.length})"),
               leading: Padding(
                 padding: EdgeInsets.symmetric(
                     vertical: ManagerHeight.h8, horizontal: ManagerWidth.w8),
@@ -32,7 +38,15 @@ class CartScreen extends StatelessWidget {
               ),
               actions: [
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    showErrorORWarningDialog(
+                        isError: false,
+                        context: context,
+                        subtitle: ManagerStrings.removeItem,
+                        fct: () {
+                          cartProvider.clearLocalCart();
+                        });
+                  },
                   icon: const Icon(
                     Icons.delete_forever_rounded,
                     color: Colors.red,
@@ -40,11 +54,27 @@ class CartScreen extends StatelessWidget {
                 ),
               ],
             ),
-            body: ListView.builder(
-              itemCount: 15,
-              itemBuilder: (context, index) {
-                return const CartWidget();
-              },
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: cartProvider.getCartItems.length,
+                    itemBuilder: (context, index) {
+                      return ChangeNotifierProvider.value(
+                        value: cartProvider.getCartItems.values
+                            .toList()
+                            .reversed
+                            .toList()[index],
+                        child: const CartWidget(),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: kBottomNavigationBarHeight +
+                      Constant.cartBottomCheckoutHeight,
+                )
+              ],
             ),
           );
   }
