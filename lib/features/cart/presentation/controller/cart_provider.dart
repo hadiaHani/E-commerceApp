981cf5c922397edb01bef86a6a0d59c9be3770da
hadiaHani <hh7113404@gmail.com>
2023-07-22 22:36:57 +0300

    CartProvider

---
+++ lib/features/cart/presentation/controller/cart_provider.dart
@@ -0,0 +1,73 @@
+import 'package:ecommerce_admin_app_firebase/features/home/domain/model/cart_model.dart';
+import 'package:ecommerce_admin_app_firebase/features/home/domain/model/product_model.dart';
+import 'package:ecommerce_admin_app_firebase/features/search/presentation/controller/product_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+class CartProvider with ChangeNotifier {
+  final Map<String, CartModel> _cartItems = {};
+
+  Map<String, CartModel> get getCartItems {
+    return _cartItems;
+  }
+
+  bool isProductInCart({required String productId}) {
+    return _cartItems.containsKey(productId);
+  }
+
+  void addProductToCart({required String productId}) {
+    _cartItems.putIfAbsent(
+      productId,
+      () => CartModel(
+        cartId: const Uuid().v4(),
+        productId: productId,
+        quantity: 1,
+      ),
+    );
+    notifyListeners();
+  }
+
+  void updateQuantity({required String productId, required int quantity}) {
+    _cartItems.update(
+      productId,
+      (item) => CartModel(
+        cartId: item.cartId,
+        productId: productId,
+        quantity: quantity,
+      ),
+    );
+    notifyListeners();
+  }
+
+  double getTotal({required ProductProvider productProvider}) {
+    double total = 0.0;
+    _cartItems.forEach((key, value) {
+      final ProductModel? getCurrProduct =
+          productProvider.findByProdId(value.productId);
+      if (getCurrProduct == null) {
+        total += 0;
+      } else {
+        total += double.parse(getCurrProduct.productPrice) * value.quantity;
+      }
+    });
+    return total;
+  }
+
+  int getQty() {
+    int total = 0;
+    _cartItems.forEach((key, value) {
+      total += value.quantity;
+    });
+    return total;
+  }
+
+  void removeOneItem({required String productId}) {
+    _cartItems.remove(productId);
+    notifyListeners();
+  }
+
+  void clearLocalCart() {
+    _cartItems.clear();
+    notifyListeners();
+  }
+}
