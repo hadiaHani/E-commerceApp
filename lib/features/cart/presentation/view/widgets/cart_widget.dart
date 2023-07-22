71ee09bdaeaa3c77b1965b157db7103cdfd31947
hadiaHani <hh7113404@gmail.com>
2023-07-22 22:38:55 +0300

    CartWidget

--- lib/features/cart/presentation/view/widgets/cart_widget.dart
+++ lib/features/cart/presentation/view/widgets/cart_widget.dart
@@ -4,10 +4,15 @@ import 'package:ecommerce_admin_app_firebase/core/resources/manager_fonts.dart';
 import 'package:ecommerce_admin_app_firebase/core/resources/manager_size.dart';
 import 'package:ecommerce_admin_app_firebase/core/widgets/subtitle_text.dart';
 import 'package:ecommerce_admin_app_firebase/core/widgets/title_text.dart';
+import 'package:ecommerce_admin_app_firebase/features/cart/presentation/controller/cart_provider.dart';
 import 'package:ecommerce_admin_app_firebase/features/cart/presentation/view/widgets/quantity_btm_sheet.dart';
+import 'package:ecommerce_admin_app_firebase/features/heart_btn.dart';
+import 'package:ecommerce_admin_app_firebase/features/home/domain/model/cart_model.dart';
+import 'package:ecommerce_admin_app_firebase/features/search/presentation/controller/product_provider.dart';
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:provider/provider.dart';
 
 class CartWidget extends StatelessWidget {
   const CartWidget({super.key});
@@ -15,105 +20,117 @@ class CartWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return FittedBox(
-      child: IntrinsicWidth(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-              vertical: ManagerHeight.h8, horizontal: ManagerWidth.w8),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(ManagerRadius.r12),
-                child: FancyShimmerImage(
-                  imageUrl:
-                      'https://i.ibb.co/8r1Ny2n/20-Nike-Air-Force-1-07.png',
-                  height: size.height * Constant.d_2,
-                  width: size.height * Constant.d_2,
-                ),
-              ),
-              SizedBox(
-                width: ManagerWidth.w10,
-              ),
-              IntrinsicWidth(
-                child: Column(
+    final cartModelProvider = Provider.of<CartModel>(context);
+    final productProvider = Provider.of<ProductProvider>(context);
+    final getCurrProduct =
+        productProvider.findByProdId(cartModelProvider.productId);
+    final cartProvider = Provider.of<CartProvider>(context);
+
+    return getCurrProduct == null
+        ? const SizedBox.shrink()
+        : FittedBox(
+            child: IntrinsicWidth(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: ManagerHeight.h8, horizontal: ManagerWidth.w8),
+                child: Row(
                   children: [
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: size.width * Constant.d_6,
-                          child: TitlesTextWidget(
-                            label: "Title" * Constant.d_10,
-                            maxLines: Constant.titleMaxLines,
-                          ),
-                        ),
-                        Column(
-                          children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.clear,
-                                color: ManagerColors.redColor,
-                              ),
-                            ),
-                            IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                IconlyLight.heart,
-                                color: ManagerColors.redColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(ManagerRadius.r12),
+                      child: FancyShimmerImage(
+                        imageUrl: getCurrProduct.productImage,
+                        height: size.height * Constant.d_2,
+                        width: size.height * Constant.d_2,
+                      ),
                     ),
-                    Row(
-                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SubtitleTextWidget(
-                          label: "16\$",
-                          fontSize: ManagerFontSize.s20,
-                          color: ManagerColors.blueColor,
-                        ),
-                        const Spacer(),
-                        OutlinedButton.icon(
-                          style: OutlinedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.circular(ManagerRadius.r30),
-                            ),
-                            side: BorderSide(
-                              width: ManagerWidth.w2,
-                              color: ManagerColors.blueColor,
-                            ),
-                          ),
-                          onPressed: () async {
-                            await showModalBottomSheet(
-                              backgroundColor:
-                                  Theme.of(context).scaffoldBackgroundColor,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(ManagerRadius.r20),
-                                  topRight: Radius.circular(ManagerRadius.r20),
+                    SizedBox(
+                      width: ManagerWidth.w10,
+                    ),
+                    IntrinsicWidth(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: size.width * Constant.d_6,
+                                child: TitlesTextWidget(
+                                  label: getCurrProduct.productTitle,
+                                  maxLines: Constant.titleMaxLines,
                                 ),
                               ),
-                              context: context,
-                              builder: (context) {
-                                return const QuantityBottomSheetWidget();
-                              },
-                            );
-                          },
-                          icon: const Icon(IconlyLight.arrowDown2),
-                          label: const Text("Qty: 6 "),
-                        ),
-                      ],
-                    )
+                              Column(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {
+                                      cartProvider.removeOneItem(
+                                        productId: getCurrProduct.productId,
+                                      );
+                                    },
+                                    icon: const Icon(
+                                      Icons.clear,
+                                      color: ManagerColors.redColor,
+                                    ),
+                                  ),
+                                  HeartButtonWidget(
+                                    productId: getCurrProduct.productId,
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          Row(
+                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SubtitleTextWidget(
+                                label: "${getCurrProduct.productPrice}\$",
+                                fontSize: ManagerFontSize.s20,
+                                color: ManagerColors.blueColor,
+                              ),
+                              const Spacer(),
+                              OutlinedButton.icon(
+                                style: OutlinedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        ManagerRadius.r30),
+                                  ),
+                                  side: BorderSide(
+                                    width: ManagerWidth.w2,
+                                    color: ManagerColors.blueColor,
+                                  ),
+                                ),
+                                onPressed: () async {
+                                  await showModalBottomSheet(
+                                    backgroundColor: Theme.of(context)
+                                        .scaffoldBackgroundColor,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft:
+                                            Radius.circular(ManagerRadius.r20),
+                                        topRight:
+                                            Radius.circular(ManagerRadius.r20),
+                                      ),
+                                    ),
+                                    context: context,
+                                    builder: (context) {
+                                      return QuantityBottomSheetWidget(
+                                        cartModel: cartModelProvider,
+                                      );
+                                    },
+                                  );
+                                },
+                                icon: const Icon(IconlyLight.arrowDown2),
+                                label:
+                                    Text("Qty: ${cartModelProvider.quantity} "),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
