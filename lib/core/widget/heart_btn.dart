fb1887fad7a9b7372645e5e7039be63fd618424d
hadiaHani <hh7113404@gmail.com>
2023-07-24 18:49:14 +0300

    HeartButtonWidget

---
+++ lib/core/widget/heart_btn.dart
@@ -0,0 +1,52 @@
+import 'package:ecommerce_admin_app_firebase/core/resources/manager_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:provider/provider.dart';
+
+import '../../features/profile/presentation/controller/wishlist_provider.dart';
+
+class HeartButtonWidget extends StatefulWidget {
+  const HeartButtonWidget({
+    super.key,
+    this.size = 22,
+    this.color = Colors.transparent,
+    required this.productId,
+  });
+  final double size;
+  final Color color;
+  final String productId;
+
+  @override
+  State<HeartButtonWidget> createState() => _HeartButtonWidgetState();
+}
+
+class _HeartButtonWidgetState extends State<HeartButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final wishlistProvider = Provider.of<WishlistProvider>(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: widget.color,
+      ),
+      child: IconButton(
+        style: IconButton.styleFrom(
+          shape: const CircleBorder(),
+        ),
+        onPressed: () {
+          wishlistProvider.addOrRemoveFromWishlist(productId: widget.productId);
+        },
+        icon: Icon(
+            wishlistProvider.isProductInWishlist(productId: widget.productId)
+                ? IconlyBold.heart
+                : IconlyLight.heart,
+            size: widget.size,
+            color: wishlistProvider.isProductInWishlist(
+                    productId: widget.productId)
+                ? ManagerColors.redColor
+                : ManagerColors.greyColor),
+      ),
+    );
+  }
+}
