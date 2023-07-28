c112fb1d635f66db9dae2b4fc5526eec4c8e682c
hadiaHani <hh7113404@gmail.com>
2023-07-28 15:17:12 +0300

    add SearchGController to widget

--- lib/features/search/presentation/view/widgets/product_widget.dart
+++ lib/features/search/presentation/view/widgets/product_widget.dart
@@ -8,121 +8,126 @@ import 'package:ecommerce_admin_app_firebase/core/widgets/title_text.dart';
 import 'package:ecommerce_admin_app_firebase/features/cart/presentation/controller/cart_provider.dart';
 import 'package:ecommerce_admin_app_firebase/core/widget/heart_btn.dart';
 import 'package:ecommerce_admin_app_firebase/features/profile/presentation/controller/viewed_prod_provider.dart';
-import 'package:ecommerce_admin_app_firebase/features/search/presentation/controller/product_provider.dart';
+import 'package:ecommerce_admin_app_firebase/features/search/presentation/controller/search_controller.dart';
 import 'package:ecommerce_admin_app_firebase/routes/routes.dart';
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 Widget productWidget(
-    {required BuildContext context, required String productId}) {
+    {required BuildContext context,
+    required String productId,
+    required SearchGController controller1}) {
   Size size = MediaQuery.of(context).size;
-
-  final productProvider = Provider.of<ProductProvider>(context);
-  final getCurrProduct = productProvider.findByProdId(productId);
-  final cartProvider = Provider.of<CartProvider>(context);
-  final viewedProvider = Provider.of<ViewedProdProvider>(context);
+  final getCurrProduct = controller1.findByProdId(productId);
 
   return getCurrProduct == null
       ? const SizedBox.shrink()
-      : Padding(
-          padding: EdgeInsets.symmetric(
-              vertical: ManagerHeight.h3, horizontal: ManagerWidth.w3),
-          child: GestureDetector(
-            onTap: () {},
-            child: InkWell(
-              onTap: () {
-                viewedProvider.addProductToHistory(
-                    productId: getCurrProduct.productId);
-                CacheData().setProductID(getCurrProduct.productId);
-                Navigator.pushNamed(
-                  context,
-                  Routes.productDetailesView,
-                );
-              },
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(ManagerRadius.r30),
-                    child: FancyShimmerImage(
-                      imageUrl: getCurrProduct.productImage,
-                      width: double.infinity,
-                      height: size.height * Constant.d_22,
+      : GetBuilder<SearchGController>(builder: (controllerds) {
+          final cartProvider = Provider.of<CartProvider>(context);
+          final viewedProvider = Provider.of<ViewedProdProvider>(context);
+
+          return Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: ManagerHeight.h3, horizontal: ManagerWidth.w3),
+            child: GestureDetector(
+              onTap: () {},
+              child: InkWell(
+                onTap: () {
+                  viewedProvider.addProductToHistory(
+                      productId: getCurrProduct.productId);
+                  CacheData().setProductID(getCurrProduct.productId);
+                  Navigator.pushNamed(
+                    context,
+                    Routes.productDetailesView,
+                  );
+                },
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(ManagerRadius.r30),
+                      child: FancyShimmerImage(
+                        imageUrl: getCurrProduct.productImage,
+                        width: double.infinity,
+                        height: size.height * Constant.d_22,
+                      ),
                     ),
-                  ),
-                  Row(
-                    children: [
-                      Flexible(
-                        flex: Constant.titleFlexible,
-                        child: TitlesTextWidget(
-                          fontSize: ManagerFontSize.s20,
-                          label: getCurrProduct.productTitle,
-                          maxLines: 2,
-                        ),
-                      ),
-                      Flexible(
-                        flex: 2,
-                        child: HeartButtonWidget(
-                            productId: getCurrProduct.productId),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: ManagerHeight.h15,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: ManagerWidth.w04),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    Row(
                       children: [
                         Flexible(
-                          flex: Constant.priceFlexible,
-                          child: SubtitleTextWidget(
-                            label: "${getCurrProduct.productPrice}\$",
+                          flex: Constant.titleFlexible,
+                          child: TitlesTextWidget(
+                            fontSize: ManagerFontSize.s20,
+                            label: getCurrProduct.productTitle,
+                            maxLines: 2,
                           ),
                         ),
                         Flexible(
-                          child: Material(
-                            borderRadius:
-                                BorderRadius.circular(ManagerRadius.r16),
-                            color: ManagerColors.lightBlueColor,
-                            child: InkWell(
-                              splashColor: ManagerColors.redColor,
+                          flex: 2,
+                          child: HeartButtonWidget(
+                              productId: getCurrProduct.productId),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: ManagerHeight.h15,
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: ManagerWidth.w04),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            flex: Constant.priceFlexible,
+                            child: SubtitleTextWidget(
+                              label: "${getCurrProduct.productPrice}\$",
+                            ),
+                          ),
+                          Flexible(
+                            child: Material(
                               borderRadius:
                                   BorderRadius.circular(ManagerRadius.r16),
-                              onTap: () {
-                                if (cartProvider.isProductInCart(
-                                    productId: getCurrProduct.productId)) {
-                                  return;
-                                }
-                                cartProvider.addProductToCart(
-                                    productId: getCurrProduct.productId);
-                              },
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: ManagerHeight.h8,
-                                    horizontal: ManagerWidth.w8),
-                                child: Icon(
-                                  cartProvider.isProductInCart(
-                                          productId: getCurrProduct.productId)
-                                      ? Icons.check
-                                      : Icons.add_shopping_cart_rounded,
-                                  size: ManagerSize.s20,
-                                  color: ManagerColors.whiteColor,
+                              color: ManagerColors.lightBlueColor,
+                              child: InkWell(
+                                splashColor: ManagerColors.redColor,
+                                borderRadius:
+                                    BorderRadius.circular(ManagerRadius.r16),
+                                onTap: () {
+                                  if (cartProvider.isProductInCart(
+                                      productId: getCurrProduct.productId)) {
+                                    return;
+                                  }
+                                  cartProvider.addProductToCart(
+                                      productId: getCurrProduct.productId);
+                                },
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: ManagerHeight.h8,
+                                      horizontal: ManagerWidth.w8),
+                                  child: Icon(
+                                    cartProvider.isProductInCart(
+                                            productId: getCurrProduct.productId)
+                                        ? Icons.check
+                                        : Icons.add_shopping_cart_rounded,
+                                    size: ManagerSize.s20,
+                                    color: ManagerColors.whiteColor,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: ManagerHeight.h10,
-                  ),
-                ],
+                    SizedBox(
+                      height: ManagerHeight.h10,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
+          );
+        });
 }
