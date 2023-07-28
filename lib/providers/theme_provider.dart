9da902408f36aa869d01e3f2d22cc4c92742e49b
hadiaHani <hh7113404@gmail.com>
2023-07-28 15:16:32 +0300

    edit ThemeProvider

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,40 +1,13 @@
 import 'package:ecommerce_admin_app_firebase/config/constant.dart';
-import 'package:ecommerce_admin_app_firebase/core/cache/cache.dart';
-import 'package:ecommerce_admin_app_firebase/features/cart/presentation/view/cart_screen.dart';
-import 'package:ecommerce_admin_app_firebase/features/home/presentation/view/home_screen.dart';
-import 'package:ecommerce_admin_app_firebase/features/profile/presentation/view/profile_screen.dart';
-import 'package:ecommerce_admin_app_firebase/features/search/presentation/view/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
-  late PageController controller;
-  int currentScreen = 0;
-  List<Widget> screens = [
-    const HomeScreen(),
-    const SearchScreen(),
-    const CartScreen(),
-    const ProfileScreen()
-  ];
-  void initState() {
-    controller = PageController(
-      initialPage: currentScreen,
-    );
-  }
-
-  onDestinationSelected(int index) {
-    currentScreen = index;
-    CacheData().setName("");
-
-    notifyListeners();
-  }
-
   bool _darkTheme = false;
   bool get getIsDarkTheme => _darkTheme;
 
   ThemeProvider() {
     getTheme();
-    initState();
   }
   Future<void> setDarkTheme({required bool themeValue}) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
